0b8ee12d2d6540187a3ee77c203b0537543e932c
Darkgobbo1337 <74969469+Darkgobbo1337@users.noreply.github.com>
2020-11-24 15:00:12 -0600

    Update disease_alchemy.lua

--- disease_alchemy.lua
+++ disease_alchemy.lua
@@ -1,1 +1,36 @@
+Disease Infusion 
+Use mode: Sustained
+Mana Cost: 20
+Range: Melee/Personal
+Cooldown: 8
+Use Speed: Spell
+When you use your abilities you infuse them with Blight damage that diseases your foe. 
+You increase your Blight damage by (7%-30%). At rank 3 and higher you increase your Blight resistance penetration by (5%-15%).
+You cannot have more than one alchemist infusion sustain active at once.
+
+Gem Crystals
+Use mode: Activated
+Mana Cost: 10
+Gem Cost: 4
+Range: 5
+Cooldown: (15-6)
+Use Speed: Spell
+You imbue a few gems with magic and toss them out to summon (2-5) crystals in a radius 3 around your target. These crystals taunt enemies around them and when destroyed they explode dealing (30-300) Infusion damage to all immediately around them in radius (1-2).
+The damage increases with spellpower and quality of the gems used.
+
+Contagion Echoes
+Use mode: Activated
+Stamina Cost: 10
+Mana Cost: 5
+Range: Melee/personal
+Cooldown: 10
+Use Speed: Weapon
+You strike out with both your weapon (1 - 1.8) and your shield (1.2 - 2.0) each dealing Blight damage in a radius 1 splash. Each splash has a chance of inflicting a random disease on the targets hit by it.
+The chance to inflict diseases increases with your spellpower.
+
+Reaving Form
+Use mode: Passive
+You borrow from the corrupt and become a carrier of magical illness and corrupted strength. While Disease Infusion is active you automatically make a shield bash for (.7-1.1) blight damage after casting a spell that uses an alchemist gem. If the target has any diseases the duration is increased by (1-3)
+Also your golem gains the bonus of your Disease Infusion and some of your golem’s talents splash in radius (1-3) with infectious blight damage.
+The damage will increase with spellpower.
 
