e16caba92d2360716ba52ac7f74a6ae6d336af6d
Darkgobbo1337 <74969469+Darkgobbo1337@users.noreply.github.com>
2020-11-24 15:01:48 -0600

    Update celestial_alchemy.lua

--- celestial_alchemy.lua
+++ celestial_alchemy.lua
@@ -1,1 +1,35 @@
+Celestial Infusion 
+Use mode: Sustained
+Mana Cost: 20
+Range: Melee/Personal
+Cooldown: 8
+Use Speed: Spell
+When you use your abilities you infuse them with Light damage that blinds your foe. 
+You increase your Light damage by (7%-30%). At rank 3 and higher you increase your Light resistance Penetration by (5%-15%).
+You cannot have more than one alchemist infusion sustain active at once.
+
+Gem Shield 
+Use mode: Activated
+Mana Cost: 40
+Gem Cost: 5
+Range: Melee/Personal
+Cooldown: (18-12)
+Use Speed: Spell
+You crush alchemist gems and spread the dust around you forming a shield to protect you from (150-400)damage for 5 turns. The first time you are attacked you send out a beam dealing (25-100) Infusion damage at your attacker to range 5. This beam always goes as far as it can even when struck in melee.
+The damage and shield power are increased by spellpower and quality of gem used.
+
+Blinding Strikes
+Use mode: Activated
+Stamina Cost: 20
+Mana Cost: 15
+Range: Melee/personal
+Cooldown: 10
+Use Speed: Shield
+You strike with shield dealing (.8-1.5) shield damage as blinding light damage. You follow up with a weapon strike with a (3%-15%) increased chance of a crit and (20%-60%) crit power if the target is blinded. 
+
+Paladin Form: 
+Use mode: Passive
+You borrow from the divine and become a beacon of light shining hope into your allies and despair into your foes. While Celestial Infusion is active you deal (10-60) light damage on all of your strikes. If you have a temporary damage shield up it will increase its power by (5-30) once per turn and add (1-3) to the duration. The duration can only be added to a damage shield once.
+Also your golem gains the bonus of your Celestial Infusion and some of your golem’s talents lite the tile of its target and all adjacent tiles while doing blinding light damage..
+The damage and power increase will increase with spellpower.
 
