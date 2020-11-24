30449714ccb72d154d8e78f0d9f1221f7a2317cd
Darkgobbo1337 <74969469+Darkgobbo1337@users.noreply.github.com>
2020-11-24 14:59:22 -0600

    Update teamwork.lua

--- teamwork.lua
+++ teamwork.lua
@@ -1,1 +1,38 @@
+High and Low
+Use mode: Activated
+Stamina Cost: 8
+Mana Cost: 4
+Range: Melee/Personal
+Cooldown: 6
+Use Speed: Weapon
+You attack your enemy dealing weapon damage (.8-1.6)and attempt to stun them. If your golem is within melee range of the same target they will attack dealing weapon damage (1-1.9) infusion damage  and attempt to pin them. If both attacks hit you gain an additional shield bash(1.5-2.1) which is an automatic critical.
+The damage is increased by your Strength.
+
+    
+    Arms and Armor 
+Use mode: Passive
+You strengthen your golem in offence and defense. Every rank of this talent increases the golems strength with weapons and defense with armor. Each rank gives a rank of golem power and golem resilience.
+
+    
+    Flying Leap 
+Use mode: Activated
+Stamina Cost: 15
+Mana Cost: 5
+Range: 3-6
+Cooldown: 7
+Use Speed: Weapon
+You leap to a space (3-6) spaces away. If your golem is adjacent to you when you activate this talent you are instead thrown (5-9) spaces away and make a (.7- 1.5) weapon attack against all adjacent enemies when you land which  has a chance to stun them for (2-5) turns.
+If your golem is not next to you it teleports next to you when you land.
+The damage and chance to stun increase with Strength.
+
+        
+        Hammer and Anvil
+Use mode: Activated
+Stamina Cost: 20
+Mana Cost: 35
+Range:Melee/personal
+Cooldown:
+Use Speed: Shield
+You slam your shield into your foe for (.6-1.2) shield damage causing them to drop their guard. Your golem follows up with an extra strong attack that takes advantage of their weakened state to deal (1.5-2.2) weapon damage.
+The damage of the initial strike is increased by strength.
 
