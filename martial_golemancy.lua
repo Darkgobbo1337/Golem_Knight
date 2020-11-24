be5b3ec4b9b6652f8ed31b15e7f4d3663c4c1907
Darkgobbo1337 <74969469+Darkgobbo1337@users.noreply.github.com>
2020-11-24 15:04:07 -0600

    Update martial_golemancy.lua

--- martial_golemancy.lua
+++ martial_golemancy.lua
@@ -1,1 +1,31 @@
+Bulldozer
+Use mode: Activated
+Mana Cost: 25
+Range: (3-8)
+Cooldown: 6
+Use Speed: Spell
+Your golem charges a target within range and slams into them dealing (.3-1.0) weapon damage and pushing them back to the full range of the dash. If they collide with terrain, destroy all terrain in radius (1-2) and deal (1.1-2.2) weapon damage to the creature and the golem. 
+As well your golem gains both Shield Pummel and Stunning Blow at a talent equal to this talent.
+
+  Gem Armor
+Use mode: Passive 
+You insert a pair of gems into your golem. Imbuing it with the benefits of the gems and increasing the golem’s armor by (5%-40%) and max life by (5%-25).
+Gem level usable: (1-5)
+The armor and max life increase with spellpower and quality of the gems used.
+This talent can alter the world in a permanent way, as such you can never unlearn it once known.
+
+  Furious Strength
+Use mode: Activated
+Mana Cost: 40
+Range: Melee/personal
+Cooldown: (28-15)
+Use Speed: Spell
+You cause your golem to become furious, spurned by fury it strikes out at foes all around it. For the next (3-7) turns your Golem strike all foes adjacent to its target. It deals 25% more damage while thus enraged.
+If dead, your golem is brought back with (25%-60%) life.
+
+    Overloaded Golem 
+Use mode: Passive
+Your golem is brimming with power increasing your golem's life, mana and stamina regeneration rates by (1-5).
+At talent level 3 your golem gains another Inscription slot. 
+As well your golem gains the talent Disarm at the same rank of this talent.
 
