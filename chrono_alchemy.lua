b7840ae7e32891e9effa4d1b74a39d4fe851ef38
Darkgobbo1337 <74969469+Darkgobbo1337@users.noreply.github.com>
2020-11-24 15:02:39 -0600

    Update chrono_alchemy.lua

--- chrono_alchemy.lua
+++ chrono_alchemy.lua
@@ -1,1 +1,35 @@
+Chono Infusion 
+Use mode: Sustained
+Mana Cost: 20
+Range: Melee/Personal
+Cooldown: 8
+Use Speed: Spell
+When you use your abilities you infuse them with Temporal damage that confuses your foe. 
+You increase your Temporal damage by (7%-30%). At rank 3 and higher you increase your Temporal resistance Penetration by (5%-15%).
+You cannot have more than one alchemist infusion sustain active at once.
+
+Gravity Gem 
+Use mode: Activated
+Mana Cost: 40
+Gem Cost: 3
+Range: 7
+Cooldown: 25
+Use Speed: Spell
+You crush three alchemist gems into a sphere and toss it into your enemies' midst causing a gravity well to form that pulls all enemies around in radius (5-10). Enemies in radius 3 will take spellpower(25-230) Temporal damage and have a chance of being pinned.
+The damage is increased with spellpower and quality of the gem used.
+
+Destabilize Space 
+Use mode: Activated
+Mana Cost: 50
+Range:(4-10)
+Cooldown: (30-18)
+Use Speed: Spell
+You warp space and time to switch spaces with a creature within (4-10) spaces. Every other enemy within (2-5) radius is teleported randomly within 4 spaces and takes (10-75) Temporal damage.
+The damage and radius are increased with spellpower
+
+Temporal Form 
+Use mode: Passive
+You borrow from time and become entangled in both past and future. While Chrono Infusion is active you pull a temporal copy of your golem from an alternate timeline after casting a spell that uses an alchemist gem. Your golem appears next to an opponent within radius 3 and makes (1-3) melee attacks before vanishing back to it’s timeline. The golem is out of phase with normal reality and deals 50% less damage.
+Also your golem gains the bonus of your Chrono Infusion and some of your golem’s talents deal Temporal damage that tries to stun, blind, pin, or confuse the target.
+The duration and damage all increase with spellpower.
 
