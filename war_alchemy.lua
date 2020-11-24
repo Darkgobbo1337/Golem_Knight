4ae56073eb0269eab516b410bd5a61f53dc1d1b4
Darkgobbo1337 <74969469+Darkgobbo1337@users.noreply.github.com>
2020-11-24 15:03:20 -0600

    Update war_alchemy.lua

--- war_alchemy.lua
+++ war_alchemy.lua
@@ -1,1 +1,32 @@
+Gem Infusions: 
+Use mode: Sustained
+Range: Melee/Personal
+Cooldown: 25
+Use Speed: Instant
+Imbuing your armor with alchemist gems every time you are struck you regain(8%-20%) Stamina and Mana up to a max of (7-12). This effect consumes one alchemist gem per turn that it is used no matter how many times you are struck.
+The amount restored and max amount are increased with spellpower and quality of the gems used.
+
+Stone from Blood: 
+Use mode: Passive
+Your alchemy is expensive but you have found a new way to fund the war effort, the blood of your enemies! When you kill an enemy you have a (15%-60%) chance to create a gem from their blood. This makes a gem based on the talent level.
+As well you create (10%-50%) more alchemist gems when you use Create Alchemist Gems
+
+Battle Alchemy: 
+Use mode: Sustained
+Stamina Cost: 15
+Mana Cost: 15
+Range: Melee/personal
+Cooldown: 30
+Use Speed: Spell
+You infuse your armor and weapons to make them stronger and more durable. You gain an additional (15%-50%) armor and any shield you wield has (15%-50%) more block. Your weapon gains (10%-25%) of your magic to your weapon damage.
+
+Flesh like Steel: 
+Use mode: Activated
+Mana Cost: 45
+Gem Cost: 5
+Range: Melee/personal
+Cooldown: (22-16)
+Use Speed: Spell
+You crush 5 alchemist gems and swallow the dust transforming your body to steel and increasing your defensive powers. You gain a bonus (3%-20%) to Resist all and your armor is able to reduce damage from all non mind damage for (2-6) turns
+The resist and duration increase with spellpower and quality of the gems used.
 
