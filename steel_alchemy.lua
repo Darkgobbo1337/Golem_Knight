b5206b6af50e486caef52fda79757e31577bcb40
Darkgobbo1337 <74969469+Darkgobbo1337@users.noreply.github.com>
2020-11-24 19:38:50 -0600

    Update steel_alchemy.lua

--- steel_alchemy.lua
+++ steel_alchemy.lua
@@ -33,12 +33,12 @@ newTalent{
 	end,
 	info = function(self, t)
 		local daminc = t.getIncrease(self, t)
-        	local phypen = t.getResistPenalty(self, t)
+        	local pen = t.getResistPenalty(self, t)
 		return ([[When you use your abilities, you infuse them with physical damage that throws your foe off-balance.
                 You increase your Physical damage by %d%%. At rank 3 and higher you increase your Physical resistance penetration by %d%%.
                 You cannot have more than one alchemist infusion sustain active at once. 
                 Switching to another infusion is instant but puts the others on a short 3 turn cooldown.]]):
-		tformat(daminc,physpen)
+		tformat(daminc,pen)
 	end,
 }
 
