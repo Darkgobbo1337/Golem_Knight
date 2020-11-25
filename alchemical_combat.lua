a55691c449a8953015c41bd0a9dadb5a07ba60f9
Darkgobbo1337 <74969469+Darkgobbo1337@users.noreply.github.com>
2020-11-25 15:46:33 -0600

    Update alchemical_combat.lua

--- alchemical_combat.lua
+++ alchemical_combat.lua
@@ -1,1 +1,47 @@
+Alchemical-Combat:This tree focuses on the use of alchemist gems in combat.
+Gem Slash 
+Use mode: Activated
+Stamina Cost: 5
+Mana Cost: 15
+Gem Cost: 1
+Range: Melee/personal
+Cooldown: 10
+Use Speed: Weapon
+Imbuing your sword with alchemist gems you make a weapon attack that deals (.8-1.6) Infusion damage and has a special effect based on the alchemist gems used. The color of the gem determines the special effect of the attack
+Black: Chance to stun the target
+Blue: Splits off to 3 nearby enemies within 3 spaces.
+Green: Disarms the target.
+Red: Burns the target for (1.1-1.7) of the damage over 3 turns
+Stone: Knocks back the target
+Violet: Chance to confuse the target.
+White: Freezes the target.
+Yellow: Blinds the target.
+
+
+Blood Gem
+Use mode: Activated
+Stamina Cost: 15
+Mana Cost: 10
+Gem Cost: 3
+Range: 1-4
+Cooldown: 7
+Use Speed: Spell
+You crush 3 alchemist gems and draw blood from your enemies to replenish your health. You send out a cone at range 1-4 dealing Infusion damage and heals you for (5%-45%) of the damage dealt to each enemy.
+The damage increases with your spellpower and quality of gems used.
+
+
+Gem Fragments: 
+Use mode: Passive
+You cause mini explosions to happen all around when your enemies attack you and chip off fragments of gems from your armor and shield. Whenever an enemy's attack deals more than (35%-15%) damage a fragment of a gem breaks off and explodes in their face. This deals (5-25) Infusion damage and knocks them off-balance for (1-3) turn.
+The damage and duration will increase with spellpower
+
+
+Leyline of Energy: 
+Use mode: Activated
+Mana Cost: 15
+Range: Melee/personal
+Cooldown:
+Use Speed: Spell
+For (3-7) turns you and your golem are connected by pure energy. Every turn every enemy between you and your golem will take (15-75) damage. If you are adjacent to your golem instead the energy explodes outwards in a radius (1-4) burst centered on each of you dealing (.5-.9) percent normal damage. Your alchemist infusions will change what damage the beam and explosion do.
+The damage will increase with your spellpower.
 
